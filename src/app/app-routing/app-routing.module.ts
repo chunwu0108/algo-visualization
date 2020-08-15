778ab128863643e675f04dcb183c018eb2160eda
Chun Wu <chunwu0108@gmail.com>
2020-08-14 22:12:40 -0700

    Added sample sudo

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -28,7 +28,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled', 
+    })
   ],
   exports: [
     RouterModule
